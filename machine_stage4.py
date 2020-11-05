a8a98a48eeef83a79110f8a88cb3ddd7c2965f59
marydcodes <40235658+marydCodes@users.noreply.github.com>
2020-11-05 17:03:42 +0000

    started working on stage4 version of the coffee machine

---
+++ machine_stage4.py
@@ -0,0 +1,10 @@
+import math as m
+
+# print the coffee machine's state
+print('''
+The coffee machine has:
+400 ml of water
+540 ml of milk
+120 g of coffee beans
+9 disposable cups
+550 dollars of money''')
