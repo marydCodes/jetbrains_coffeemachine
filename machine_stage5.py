46585b457c64a1ef4e974210871d637e4096833e
marydcodes <40235658+marydCodes@users.noreply.github.com>
2020-11-06 19:40:44 +0000

    completed stage5

--- machine_stage5.py
+++ machine_stage5.py
@@ -1,91 +1,111 @@
-state = {"w": 400,
-         "m": 540,
-         "b": 120,
-         "c": 9,
-         "d": 550}
+# current resources
+state = {"water": 400,"milk": 540,"beans": 120,"money": 550, "cups": 9}
 
 # recipe espresso
-espresso = {"w": 250,"m": 0,"b": 16,"d": 4}
+espresso = {"water": 250,"milk": 0,"beans": 16,"money": 4}
 
 # recipe latte
-latte = {"w": 350,"m": 75,"b": 20,"d": 7}
+latte = {"water": 350,"milk": 75,"beans": 20,"money": 7}
 
 # recipe cappuccino
-cappuccino = {"w": 200,"m": 100,"b": 12,"d": 6}
+capp = {"water": 200,"milk": 100,"beans": 12,"money": 6}
 
 # process queries
-# first ask if they want to buy, fill or take
 def choose():
     user_input = str(input("Write action (buy, fill, take, remaining, exit): "))
     return user_input
 
 def buy():
-    coffee = int(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: "))
-    if coffee == 1:
+    coffee = str(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: "))
+    if coffee == "1":
+        buy_espresso()
+    elif coffee == "2":
+        buy_latte()
+    elif coffee == "3":
+        buy_capp()
+    elif coffee == "back":
+        pass
+
+def buy_espresso():
+    # Check: with amounts of [w, m, b, c] available, can we make an espresso?
+    for k in espresso:
+        if state[k] < espresso[k]:
+            print(f"Sorry, not enough {k}!")
+            break
+    else:
+        print("I have enough resources, making you a coffee!")
         # update state according to espresso
-        state["w"] -= espresso["w"]
-        state["m"] -= espresso["m"]
-        state["b"] -= espresso["b"]
-        state["c"] -= 1
-        state["d"] += espresso["d"]
-    elif coffee == 2:
-        print("You chose a latte. Pretty standard.")
-        # update state according to latte
-        state["w"] -= latte["w"]
-        state["m"] -= latte["m"]
-        state["b"] -= latte["b"]
-        state["c"] -= 1
-        state["d"] += latte["d"]
-    elif coffee == 3:
-        print("You chose a cappuccino. Foamy!")
-        # update state according to cappuccino
-        state["w"] -= cappuccino["w"]
-        state["m"] -= cappuccino["m"]
-        state["b"] -= cappuccino["b"]
-        state["c"] -= 1
-        state["d"] += cappuccino["d"]
+        state["water"] -= espresso["water"]
+        state["milk"] -= espresso["milk"]
+        state["beans"] -= espresso["beans"]
+        state["money"] += espresso["money"]
+        state["cups"] -= 1
+
+def buy_latte():
+    for k in latte:
+        if state[k] < latte[k]:
+            print(f"Sorry, not enough {k}!")
+            break
+    else:
+        print("I have enough resources, making you a coffee!")
+        state["water"] -= latte["water"]
+        state["milk"] -= latte["milk"]
+        state["beans"] -= latte["beans"]
+        state["money"] += latte["money"]
+        state["cups"] -= 1
+
+def buy_capp():
+    for k in capp:
+        if state[k] < capp[k]:
+            print(f"Sorry, not enough {k}!")
+            break
+    else:
+        print("I have enough resources, making you a coffee!")
+        state["water"] -= capp["water"]
+        state["milk"] -= capp["milk"]
+        state["beans"] -= capp["beans"]
+        state["money"] += capp["money"]
+        state["cups"] -= 1
 
 
 def fill():
-    add_water = int(input("Write how many ml of water do you want to add: "))
-    add_milk = int(input("Write how many ml of milk do you want to add: "))
-    add_beans = int(input("Write how many grams of coffee beans do you want to add: "))
-    add_cups = int(input("Write how many disposable cups of coffee do you want to add: "))
-
-    state["w"] += add_water
-    state["m"] += add_milk
-    state["b"] += add_beans
-    state["c"] += add_cups
+    state["water"] += int(input("Write how many ml of water do you want to add: "))
+    state["milk"] += int(input("Write how many ml of milk do you want to add: "))
+    state["beans"] += int(input("Write how many grams of coffee beans do you want to add: "))
+    state["cups"] += int(input("Write how many disposable cups of coffee do you want to add: "))
 
 def take():
-    print(f"I gave you ${state['d']}")
-    state["d"] = 0
+    print(f"I gave you ${state['money']}")
+    state["money"] = 0
 
 def remaining():
     print(f'''
     The coffee machine has:
-    {state["w"]} ml of water
-    {state["m"]} ml of milk
-    {state["b"]} g of coffee beans
-    {state["c"]} disposable cups
-    {state["d"]} dollars of money''')
-
-def exit(): print("Goodbye")
+    {state["water"]} ml of water
+    {state["milk"]} ml of milk
+    {state["beans"]} g of coffee beans
+    {state["cups"]} disposable cups
+    ${state["money"]} dollars of money''')
 
 def machine_on():
-    user_input = choose()
+    user_input = str(input("Write action (buy, fill, take, remaining, exit): "))
     if user_input == "buy":
         buy()
+        print()
+        machine_on()
     elif user_input == "fill":
         fill()
+        print()
         machine_on()
     elif user_input == "take":
         take()
+        print()
         machine_on()
     elif user_input == "remaining":
         remaining()
+        print()
         machine_on()
     elif user_input == "exit":
-        exit()
+        print("Goodbye")
 
 machine_on()
